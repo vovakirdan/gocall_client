7b9e9a7060ae4f9a162aa1aac034922a5178f21c
vova.kirdan@mail.ru <_4IDuzhSOe>
2024-12-30 19:52:35 +0300

    mac support

--- src/services/mediaHandler.ts
+++ src/services/mediaHandler.ts
@@ -1,14 +1,17 @@
-export async function getLocalStream(): Promise<MediaStream> {
+export const getLocalStream = async (): Promise<MediaStream> => {
+  if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
     try {
       const stream = await navigator.mediaDevices.getUserMedia({
-        video: true,
         audio: true,
+        video: true,
       });
-      console.log("Local stream obtained");
       return stream;
     } catch (error) {
-      console.error("Error accessing media devices:", error);
-      throw error;
+      console.error('Error accessing media devices:', error);
+      throw new Error('Не удалось получить доступ к камере и микрофону.');
     }
+  } else {
+    console.error('navigator.mediaDevices.getUserMedia is not available');
+    throw new Error('Ваша среда выполнения не поддерживает доступ к медиаустройствам.');
   }
-  
+};
